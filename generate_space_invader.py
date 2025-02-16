ac6be112f7df482e7b3cd98bc099f61b4ebd7a1f
MeviDiRaizel <mevidiraizel@gmail.com>
2025-02-17 00:40:20 +0800

    test2

--- generate_space_invader.py
+++ generate_space_invader.py
@@ -10,7 +10,7 @@ from svgwrite import animate
 
 
 def get_contributions(username: str) -> List[dict]:
-    # GitHub GraphQL query remains the same
+    
     query = """
     query($username: String!) {
       user(login: $username) {
@@ -56,65 +56,75 @@ def create_space_invader_svg(contributions, output_file: str):
     # Add laser beam
     laser = dwg.g(id='laser')
     laser.add(dwg.line(start=(30, 160), end=(30, 20), stroke='#ff0000', stroke_width=2))
-    laser_animate = dwg.animate(attributeName='opacity', values='0;1;0', dur='0.5s', repeatCount='indefinite')
+    laser_animate = dwg.animate(
+        attributeName='opacity',
+        values='0;1;0',
+        dur='0.5s',
+        repeatCount='indefinite'
+    )
     laser.add(laser_animate)
     dwg.add(laser)
     
-    # Add contributions as targets with different colors based on count
+    # Process contribution data
     x, y = 50, 20
-    for contrib in contributions:
-        if contrib['contributionCount'] > 0:
-            # Color gradient based on contribution count
-            if contrib['contributionCount'] > 10:
-                color = '#ff0000'  # Red for high contributions
-            elif contrib['contributionCount'] > 5:
-                color = '#ffff00'  # Yellow for medium contributions
-            else:
-                color = '#00ff00'  # Green for low contributions
+    for week in contributions:
+        for day in week['contributionDays']:
+            count = day['contributionCount']
+            if count > 0:
+                color = '#ff0000' if count > 10 else '#ffff00' if count > 5 else '#00ff00'
                 
-            target = dwg.g(id=f'target-{x}-{y}')
-            target.add(dwg.rect(insert=(x, y), size=(10, 10), fill=color))
+                target = dwg.g(id=f'target-{x}-{y}')
+                target.add(dwg.rect(insert=(x, y), size=(10, 10), fill=color))
+                
+                explosion = dwg.g(id=f'explosion-{x}-{y}', opacity=0)
+                explosion.add(dwg.circle(center=(x+5, y+5), r=8, fill='#ff4500'))
+                explosion.add(dwg.animate(
+                    attributeName='opacity',
+                    values='0;1;0',
+                    dur='0.5s',
+                    begin='indefinite'
+                ))
+                dwg.add(target)
+                dwg.add(explosion)
             
-            # Add explosion animation
-            explosion = dwg.g(id=f'explosion-{x}-{y}', opacity=0)
-            explosion.add(dwg.circle(center=(x+5, y+5), r=8, fill='#ff4500'))
-            explosion.add(dwg.animate(attributeName='opacity', values='0;1;0', dur='0.5s', begin='indefinite'))
-            dwg.add(explosion)
-            
-            dwg.add(target)
-        x += 15
-        if x > 850:
-            x = 50
-            y += 15
+            x += 15
+            if x > 850:
+                x = 50
+                y += 15
     
-    # Spaceship movement animation
-    spaceship_animate = dwg.animate()
-    spaceship_animate['attributeName'] = 'transform'
-    spaceship_animate['values'] = 'translate(0,0); translate(800,0); translate(0,0)'
-    spaceship_animate['dur'] = '8s'
-    spaceship_animate['repeatCount'] = 'indefinite'
+    # Add spaceship animation
+    spaceship_animate = dwg.animate(
+        attributeName='transform',
+        values='translate(0,0);translate(800,0);translate(0,0)',
+        dur='8s',
+        repeatCount='indefinite'
+    )
     spaceship.add(spaceship_animate)
-    
-    # Add spaceship to drawing
     dwg.add(spaceship)
     
-    # Add some stars in background
+    # Add stars
     for _ in range(50):
         x = random.randint(0, 900)
         y = random.randint(0, 200)
         star = dwg.circle(center=(x, y), r=1, fill='#ffffff')
-        star_animate = dwg.animate(attributeName='opacity', values='0.2;1;0.2', dur='2s', repeatCount='indefinite')
+        star_animate = dwg.animate(
+            attributeName='opacity',
+            values='0.2;1;0.2',
+            dur='2s',
+            repeatCount='indefinite'
+        )
         star.add(star_animate)
         dwg.add(star)
     
+    # Save the SVG file
     dwg.save()
 
 if __name__ == "__main__":
     username = sys.argv[1] if len(sys.argv) > 1 else "MeviDiRaizel"
     data = get_contributions(username)
-    create_space_invader_svg(
-        data['data']['user']['contributionsCollection']['contributionCalendar']['weeks'],
-        'contribution_space_invader.svg'
-        
-        
-    )
+    
+    if 'data' in data and 'user' in data['data']:
+        weeks = data['data']['user']['contributionsCollection']['contributionCalendar']['weeks']
+        create_space_invader_svg(weeks, 'contribution_space_invader.svg')
+    else:
+        print("Error: Could not fetch contribution data")
